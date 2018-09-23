f252d0aa7020a817e169914324629be98487183b
Dinesh <ldineshkumar85@yahoo.com>
2018-09-23 21:55:36 +0200

    Balanced Binary Tree - sol 2

--- src/TreeNode.java
+++ src/TreeNode.java
@@ -63,13 +63,21 @@ public class TreeNode {
 		
 		return true;
 	}
-	
+
+	@Override
+	public String toString() {
+		return "TreeNode{" +
+				"data=" + data +
+				'}';
+	}
+
 	public int height() {
 		int leftHeight = left != null ? left.height() : 0;
 		int rightHeight = right != null ? right.height() : 0;
 		return 1 + Math.max(leftHeight, rightHeight);
 	}
-	
+
+
 	public TreeNode find(int d) {
 		if (d == data) {
 			return this;
